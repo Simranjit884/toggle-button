e39ebe41b63c146206ac437fc50e398690698de7
samy884 <ssgill884@gmail.com>
2022-05-31 14:17:02 +0530

    src changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import UserFinder from './components/UserFinder';
+import React from 'react';
+import BackwardCounter from './components/BackwardCounter';
+import ForwardCounter from './components/ForwardCounter';
 
 function App() {
   return (
-    <div>
-      <UserFinder />
-    </div>
+    <React.Fragment>
+      <ForwardCounter />
+      <BackwardCounter />
+    </React.Fragment>
   );
 }
 
