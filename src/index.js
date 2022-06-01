3a51f85158e15866ae79d3565ee4e3487f35efff
samy884 <ssgill884@gmail.com>
2022-06-01 11:17:39 +0530

    creating new src folder for https

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-import App from './App';
+import App from 'c:/Users/hp/Downloads/04-onwards-to-a-more-realistic-example/04-onwards-to-a-more-realistic-example/src/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
